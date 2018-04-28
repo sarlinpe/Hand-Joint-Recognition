2d4bf379dab73f4f8e031fa91efa56b9f5ca99ef
Moritz Zimmermann <zimoritz@ethz.ch>
2018-04-28 12:46:23 +0200

    Rework scope strucutre in resnet_baseline model. Fix naming bugs

--- jointrecog/models/resnet_baseline.py
+++ jointrecog/models/resnet_baseline.py
@@ -1,69 +1,69 @@
-lmport tensorflow as tf
-from tensorflow import layers as tfl
+import tensorflow as tf
+from tensorflow.contrib import slim
 
 from .base_model import BaseModel, Mode
 from .backbones import resnet_v2 as resnet
 
 NUM_JOINTS = 21
 
-class JointRecognition(BaseModel):
+class ResnetBaseline(BaseModel):
     input_spec = {
             'image': {'shape': [None, None, None, 3], 'type': tf.float32},
-            'scoremap' : {'shape' : [None, None, None], 'type': tf.float32},
-            'keypoints' : {'shape' : [None, NUM_JOINTS, 2], 'type': tf.float32}
+            'scoremap' : {'shape': [None, None, None, 1], 'type': tf.float32},
+            'keypoints' : {'shape': [None, NUM_JOINTS, 2], 'type': tf.float32},
+            'disks' : {'shape': [None, None, None, 1], 'type': tf.float32}
     }
-    required_config_keys = []
-    default_config = {'data_format': 'channels_first'}
 
-    def jointrecog_model(self, inputs, mode, **config):
-        x = inputs['image']
-        shape = tf.shape(inputs['image'])[1:3]
-        if config['data_format'] == 'channels_first':
-            x = tf.transpose(x, [0, 3, 1, 2])
+    default_config = { 'data': {'data_format': 'channels_last'}, 'model': {'output_kernel': [3, 3]}}
 
-        # Resnet Backbone
-        with slim.arg_scope(resnet.resnet_arg_scope()):
-            is_training = Mode.TRAIN
+    def _model(self, inputs, mode, **config):
+        image = inputs['image']
+        print(inputs.keys())
+        if config['data']['data_format'] == 'channels_first':
+            image = tf.transpose(image, [0, 3, 1, 2])
+
+        with tf.name_scope('prediction'):
+
+            is_training = (mode == Mode.TRAIN)
+            shape = tf.shape(image)[1:3]
+
             with slim.arg_scope([slim.conv2d, slim.batch_norm], trainable=is_training):
-                _, encoder = resnet.resnet_v2_50(image,
+                with slim.arg_scope(resnet.resnet_arg_scope()):
+                    # Resnet Backbone
+                    _, encoder = resnet.resnet_v2_50(image,
                                              is_training=is_training,
                                              global_pool=False,
                                              scope='resnet_v2_50')
-        feature_map = encoder['resnet_v2_50/block3']
+                    feature_map = encoder['resnetbaseline/resnet_v2_50/block3']
 
-        # Upsampling of feature map to input size
-        with slim.arg_scope(resnet.resnet_arg_scope()):
-            is_training = config['train_backbone'] and (mode == Mode.TRAIN)
-            with slim.arg_scope([slim.conv2d, slim.batch_norm], trainable=is_training):
-                # Resize feature map to match label dimensions
-                x = tf.image.resize_bilinear(x, shape)
+                # Upsampling of feature map to input size
+                image = tf.image.resize_bilinear(image, [shape[0], shape[1]])
                 # Apply convolution to obtain one tensor slice per keypoint
-                x = slim.conv2d(
-                        x, NUM_JOINTS, kernel_size = config['output_kernel'],
-                        rate = 1.0, activation_fn = None, scope = 'convOut')
+                scores = slim.conv2d(
+                        image, NUM_JOINTS, kernel_size = config['model']['output_kernel'],
+                        activation_fn = None, scope = 'convOut')
 
-        with slim.name_scope('prediction'):
             x_grid = tf.to_float(tf.expand_dims(tf.expand_dims(
                 tf.tile(tf.expand_dims(tf.range(shape[0]), 1), [1, shape[1]]), -1), 0))
             y_grid = tf.to_float(tf.expand_dims(tf.expand_dims(
                 tf.tile(tf.expand_dims(tf.range(shape[1]), 0), [shape[0], 1]), -1), 0))
 
-            with slim.name_scope('expectation'):
+            with tf.name_scope('expectation'):
                 # Predict joint locations from expectation values of scoremaps
                 # Calculate expectation as weighted avg of grid * scoremap
-                expectation_x = tf.reduce_sum(x_grid * outputs['scoremap'], axis=[1, 2]))
-                expectation_y = tf.reduce_sum(y_grid * outputs['scoremap'], axis=[1, 2]))
-                expectations_x /= tf.reduce_sum(outputs['scoremap'], axis=[1,2])
-                expectations_y /= tf.reduce_sum(outputs['scoremap'], axis=[1,2])
+                expectations_x = tf.reduce_sum(x_grid * scores, axis=[1, 2])
+                expectations_y = tf.reduce_sum(y_grid * scores, axis=[1, 2])
+                expectations_x /= tf.reduce_sum(scores, axis=[1,2])
+                expectations_y /= tf.reduce_sum(scores, axis=[1,2])
                 expectations = tf.stack([expectations_x, expectations_y], 2)
 
-            with slim.name_scope('maxscore'):
+            with tf.name_scope('maxscore'):
                 # Predict joint locations from pixels with maximum score
-                mask = tf.equal(outputs['score'], tf.reduce_max(outputs['score'],
-                    axis= [1, 2], keepdims=True)
-                max_scores = tf.reduce_max(tf.to_float(mask) * x_gridi, axis=[1, 2i, axis=[1, 2]])
+                mask = tf.equal(scores,
+                    tf.reduce_max(scores, axis=[1, 2], keepdims=True))
+                max_scores = tf.reduce_max(tf.to_float(mask) * x_grid, axis=[1, 2])
 
-        return {'scoremap': x,
+        return {'scoremap': scores,
                 'expectations' : expectations,
                 'maxscores' : max_scores}
 
@@ -76,14 +76,14 @@ class JointRecognition(BaseModel):
     def _metrics(self, outputs, inputs, **config):
         metrics = {}
         with tf.name_scope('metrics'):
-            shape = tf.shape(outputs['scoremap'])
+            shape = tf.shape(outputs['scoremap'])[1:3]
             x_grid = tf.to_float(tf.expand_dims(tf.expand_dims(
                 tf.tile(tf.expand_dims(tf.range(shape[0]), 1), [1, shape[1]]), -1), 0))
             y_grid = tf.to_float(tf.expand_dims(tf.expand_dims(
                 tf.tile(tf.expand_dims(tf.range(shape[1]), 0), [shape[0], 1]), -1), 0))
             # Calculate expectation as weighted avg of grid * scoremap
-            expectation_x = tf.reduce_sum(x_grid * outputs['scoremap'], axis=[1, 2]))
-            expectation_y = tf.reduce_sum(y_grid * outputs['scoremap'], axis=[1, 2]))
+            expectations_x = tf.reduce_sum(x_grid * outputs['scoremap'], axis=[1, 2])
+            expectations_y = tf.reduce_sum(y_grid * outputs['scoremap'], axis=[1, 2])
             expectations_x /= tf.reduce_sum(outputs['scoremap'], axis=[1,2])
             expectations_y /= tf.reduce_sum(outputs['scoremap'], axis=[1,2])
 
