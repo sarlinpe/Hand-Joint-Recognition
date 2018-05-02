cff15a44c79ebe1aebf7af6fa1284afa17aec6d1
Moritz Zimmermann <zimoritz@ethz.ch>
2018-05-02 09:51:36 +0200

    Remove duplicates in model definition of resnet_baseline

--- jointrecog/models/resnet_baseline.py
+++ jointrecog/models/resnet_baseline.py
@@ -8,19 +8,16 @@ NUM_JOINTS = 21
 
 class ResnetBaseline(BaseModel):
     input_spec = {
-            'image': {'shape': [None, None, None, 3], 'type': tf.float32},
+        'image': {'shape': [None, None, None, 3], 'type': tf.float32},
             'scoremap' : {'shape': [None, None, None, NUM_JOINTS], 'type': tf.float32},
             'keypoints' : {'shape': [None, NUM_JOINTS, 2], 'type': tf.float32},
             'disks' : {'shape': [None, None, None, NUM_JOINTS], 'type': tf.float32}
     }
 
-    default_config = { 'data': {'data_format': 'channels_last'}, 'model': {'output_kernel': [3, 3]}}
+    default_config = {'model': {'output_kernel': [3, 3]}}
 
     def _model(self, inputs, mode, **config):
         image = inputs['image']
-        print(inputs.keys())
-        if config['data']['data_format'] == 'channels_first':
-            image = tf.transpose(image, [0, 3, 1, 2])
 
         with tf.name_scope('prediction'):
 
@@ -53,8 +50,8 @@ class ResnetBaseline(BaseModel):
                 # Calculate expectation as weighted avg of grid * scoremap
                 expectations_x = tf.reduce_sum(x_grid * scores, axis=[1, 2])
                 expectations_y = tf.reduce_sum(y_grid * scores, axis=[1, 2])
-                expectations_x /= tf.reduce_sum(scores, axis=[1,2])
-                expectations_y /= tf.reduce_sum(scores, axis=[1,2])
+                expectations_x /= tf.reduce_sum(scores, axis=[1, 2])
+                expectations_y /= tf.reduce_sum(scores, axis=[1, 2])
                 expectations = tf.stack([expectations_x, expectations_y], 2)
 
             with tf.name_scope('maxscore'):
@@ -76,18 +73,6 @@ class ResnetBaseline(BaseModel):
     def _metrics(self, outputs, inputs, **config):
         metrics = {}
         with tf.name_scope('metrics'):
-            shape = tf.shape(outputs['scoremap'])[1:3]
-            x_grid = tf.to_float(tf.expand_dims(tf.expand_dims(
-                tf.tile(tf.expand_dims(tf.range(shape[0]), 1), [1, shape[1]]), -1), 0))
-            y_grid = tf.to_float(tf.expand_dims(tf.expand_dims(
-                tf.tile(tf.expand_dims(tf.range(shape[1]), 0), [shape[0], 1]), -1), 0))
-            # Calculate expectation as weighted avg of grid * scoremap
-            expectations_x = tf.reduce_sum(x_grid * outputs['scoremap'], axis=[1, 2])
-            expectations_y = tf.reduce_sum(y_grid * outputs['scoremap'], axis=[1, 2])
-            expectations_x /= tf.reduce_sum(outputs['scoremap'], axis=[1,2])
-            expectations_y /= tf.reduce_sum(outputs['scoremap'], axis=[1,2])
-
-            expectations = tf.stack([expectations_x, expectations_y], 2)
-            d = inputs['keypoints'] - expectations
+            d = inputs['keypoints'] - outputs['expectations']
             metrics['l2dist'] = tf.reduce_mean(tf.square(d))
         return metrics
